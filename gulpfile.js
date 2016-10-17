4d28fb2f4cc366ab5d0b7e9aba0a9a70bef4bd29
Justin Frasier <Justin_Frasier@me.com>
2016-10-17 10:37:59 -0500

    update

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	plumber = require('gulp-plumber');
+	
+
+
+gulp.task('scss', function () {
+	gulp.src('./scss/**/code.scss')
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(gulp.dest('./css'));
+});
+
+
+
+gulp.task('watch', function(){
+	gulp.watch('./scss/**/*.scss', ['scss']);
+});
+
+gulp.task('default',['scss',  'watch']);
