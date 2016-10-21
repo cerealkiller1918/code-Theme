e13cbfeabd367d84ff84bf61b32ced49719530b9
Justin Frasier <Justin_Frasier@me.com>
2016-10-21 08:48:43 -0500

    Changed the single quotes to double quotes

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	plumber = require('gulp-plumber'),
-	browserSync = require('browser-sync'),
-	git = require('gulp-git');
+var gulp = require("gulp"),
+	sass = require("gulp-sass"),
+	plumber = require("gulp-plumber"),
+	browserSync = require("browser-sync"),
+	git = require("gulp-git");
 
 
-gulp.task('scss', function () {
-	gulp.src('./scss/**/code.scss')
+gulp.task("scss", function () {
+	gulp.src("./scss/**/code.scss")
 		.pipe(plumber())
 		.pipe(sass())
-		.pipe(gulp.dest('./css'))
+		.pipe(gulp.dest("./css"))
 		.pipe(browserSync.reload({stream: true}));
 
 });
 
 
 
-gulp.task('watch', function(){
+gulp.task("watch", function(){
 	browserSync.init({
-		proxy:  '192.168.1.5:9094'
+		proxy:  "192.168.1.5:9094"
 	});
-	gulp.watch('./**/*.scss', ['scss']);
-	gulp.watch('./**/*.php').on('change', browserSync.reload);
+	gulp.watch("./**/*.scss", ["scss"]);
+	gulp.watch("./**/*.php").on("change", browserSync.reload);
 });
 
 
 
 
-gulp.task('default',['scss', 'watch']);
+gulp.task("default",["scss","watch"]);
